0a7d9f7e3279a576424e5040f38f9241cd197c59
Jordan Medina <jordanzet@gmail.com>
2014-02-10 20:06:43 -0500

    ejercicio #2 del capitulo 3 del libro de cordova

---
+++ cap3-2.cpp
@@ -0,0 +1,25 @@
+# include <iostream.h>
+# include <ctype.h>
+# include <>
+
+void longitud(){
+	cout<<"Ingrese el numero de Empleados: ";cin>>n_emp;
+
+}
+
+void lectura(){
+	cout<<"Ingrese Codigo: ";cin>>cod;
+	cout<<"suma de factores";
+}
+
+void modulo11(){
+
+}
+
+void main(){int n_emp;
+	char letra[]={'A','B','C','D','E','F','G','H','I','J','K'};
+	for (int i = 0; i < n_emp ; ++i){
+		lectura()
+	}
+}
+
